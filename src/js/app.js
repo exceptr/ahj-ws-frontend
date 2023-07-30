eca7de09a31ec2b60e12b9ccdc9a4f53f39ee29c
exceptr <98112445+exceptr@users.noreply.github.com>
2023-07-30 18:31:17 +0300

    init

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,6 @@
 import Chat from "./Chat";
 
-const chat = new Chat(
-  document.querySelector(".user-panel-list"),
-  "ws://ahj-ws-backend-mm9f.onrender.com/ws"
-);
+const chat = new Chat(document.querySelector('.user-panel-list'), 'ws://localhost:7070/ws');
+
+// const chat = new Chat(document.querySelector('.user-panel-list'), 'ws://ahj-ws-backend-mm9f.onrender.com/ws');
 chat.init();
